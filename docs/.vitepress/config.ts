0b125bd5124d926642fffbf7d2cde388e686d004
zuowendong <17326195691@163.com>
2023-04-04 10:14:16 +0800

    add vitepress-theme-demoblock

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,23 +1,28 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
+import { demoBlockPlugin } from "vitepress-theme-demoblock";
 
 export default defineConfig({
-  title: 'VitePress',
-  description: 'Just playing around.',
-
-  themeConfig: {
-    sidebar: {
-      "/": [
-        {text: '快速开始',link: '/'},
-        {
-          text: '通用',
-          items: [
-            {
-              text: 'Tree 树',
-              link: '/components/tree/'
-            }
-          ]
-        }
-      ]
-    }
-  }
-})
+	title: "VitePress",
+	description: "Just playing around.",
+	themeConfig: {
+		sidebar: {
+			"/": [
+				{ text: "快速开始", link: "/" },
+				{
+					text: "通用",
+					items: [
+						{
+							text: "Tree 树",
+							link: "/components/tree/",
+						},
+					],
+				},
+			],
+		},
+	},
+	markdown: {
+		config: (md) => {
+			md.use(demoBlockPlugin);
+		},
+	},
+});
--- docs/components/tree/index.md
+++ docs/components/tree/index.md
@@ -1,3 +1,29 @@
 # tree
 
-<WdTree :data="[{label: '中国菜', children: [{label: '淮扬菜'}]}]"/>
+:::demo 使用`data`定义`tree`数据
+
+```vue
+<template>
+  <WdTree :data="treeData"/>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+ setup () {
+  let treeData = ref([
+    {
+      label: '中国菜',
+      children: [{ label: '淮扬菜' }]
+    }
+  ])
+  return {
+    treeData
+  }
+ }
+})
+</script>
+```
+
+:::
